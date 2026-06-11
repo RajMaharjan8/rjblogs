eb4e553bf6283d9b71f63d7f74b0c8541de33d0e
Raj Maharjan <rajm48952@gmail.com>
2026-06-11 06:51:12 +0545

    next cache removed

--- src/services/baseQuery.tsx
+++ src/services/baseQuery.tsx
@@ -5,13 +5,29 @@ interface GraphQLRequest {
   variables?: Record<string, any>;
 }
 
-export function performRequest({ query, variables }: GraphQLRequest) {
-  const endpoint = process.env.NEXT_PUBLIC_BASE_URL ?? "";
+// export function performRequest({ query, variables }: GraphQLRequest) {
+//   const endpoint = process.env.NEXT_PUBLIC_BASE_URL ?? "";
 
-  const client = new GraphQLClient(endpoint, {
+//   const client = new GraphQLClient(endpoint, {
+//     headers: {
+//       authorization: `Bearer ${process.env.NEXT_PUBLIC_DATOCMS_API_TOKEN}`,
+//     },
+//   });
+//   return client.request(query, variables);
+// }
+
+export async function performRequest({query, variables}:GraphQLRequest){
+  const endpoint = process.env.NEXT_PUBLIC_BASE_URL ?? "";
+  const response = await fetch(endpoint, {
+    method: "POST",
     headers: {
+      "Content-Type": "application/json",
       authorization: `Bearer ${process.env.NEXT_PUBLIC_DATOCMS_API_TOKEN}`,
     },
+    body: JSON.stringify({query, variables}),
+    next: {revalidate: 60}
   });
-  return client.request(query, variables);
+
+  const json = await response.json();
+  return json.data;
 }
