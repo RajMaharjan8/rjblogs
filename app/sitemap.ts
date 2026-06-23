f59c9acc186ddebff1a4e95be776903ac8b60b4a
Raj Maharjan <rajm48952@gmail.com>
2026-06-23 12:19:10 +0545

    chore: blog seo added

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,15 +1,12 @@
 import type { MetadataRoute } from "next";
-import { fetchBlogs, fetchCategories } from "@/src/helper";
+import { fetchBlogs } from "@/src/helper";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const siteUrl = (
     process.env.NEXT_PUBLIC_SITE_URL ?? "http://localhost:3000"
   ).replace(/\/$/, "");
 
-  const [blogs, categories] = await Promise.all([
-    fetchBlogs(100, null, null),
-    fetchCategories(),
-  ]);
+  const blogs = await fetchBlogs(100, null, null);
 
   const staticRoutes: MetadataRoute.Sitemap = [
     {
@@ -33,14 +30,5 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     priority: 0.8,
   }));
 
-  const categoryRoutes: MetadataRoute.Sitemap = (categories ?? []).map(
-    (category) => ({
-      url: `${siteUrl}/${category.id}`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.6,
-    }),
-  );
-
-  return [...staticRoutes, ...blogRoutes, ...categoryRoutes];
+  return [...staticRoutes, ...blogRoutes];
 }
